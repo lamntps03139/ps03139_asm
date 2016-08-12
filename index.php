d12a6b6f7486ef0276d3d231a385500fbf84fdda
lamntps03139 <lamntps03139@fpt.edu.vn>
2016-08-12 17:59:06 +0700

    asm_ps03139

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Du Lịch Việt Nam</title>
+<link rel="stylesheet" type="text/css" href="css/bodystyles.css" />
+<link rel="stylesheet" type="text/css" href="css/menuleft.css" />
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="shortcut icon" href="anh/liva_theme_hipc_favicon.ico" type="image/x-icon" />
+</head>
+<body>
+<table width="950" border="0" align="center" bgcolor="#FFFFFF" >
+  <tr>
+    <td height="142" colspan="3" background="anh/logo.png" ><?php include "header.php";?></td>
+  </tr>
+  <tr>
+    <td height="40" colspan="3" bgcolor="#0066CC"><?php include "top.php";?></td>
+  </tr>
+  <tr>
+    <td width="150" valign="top"><?php include "left.php";?></td>
+    <td width="600" valign="top"><?php include "center.php";?></td>
+    <td width="200" valign="top"><?php include "right.php"; ?></td>
+  </tr>
+  <tr>
+    <td colspan="3" class="style_bottom" bgcolor="#0066CC"><p> Nguyễn Thanh Lâm</p>
+<p> Địa chỉ : 391A Nam Kỳ Khởi Nghĩa,Quận 3.</p>
+<p>Nguồn : http://sharecode.org/</p></td>
+  </tr>
+  
+</table>
+
+</body>
+</html>
